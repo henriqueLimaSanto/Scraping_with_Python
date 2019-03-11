d52435b27b3313db34770d086edb48ff4858b48f
henriqueLimaSanto <42854677+henriqueLimaSanto@users.noreply.github.com>
2019-03-11 02:23:55 -0300

    Add files via upload

---
+++ ExtraindoDados.py
@@ -0,0 +1,41 @@
+from urllib.request import  urlopen
+from urllib.error import HTTPError, URLError
+from bs4 import BeautifulSoup
+import  re
+
+def getElementWEb(url):
+
+    try:
+        html = urlopen(url)
+    except HTTPError as error:
+        print("Erro HTTP :",error)
+        return None
+    except URLError as error:
+        print("ocorreu um erro de Url ;",error)
+        return None
+    except AttributeError as error:
+        print("Error",error)
+        return  None
+    except:
+        print("Timeout Ocorreu um erro na pagina")
+
+    try:
+        bsObj = BeautifulSoup(html.read(),"html.parser")
+        titulo = bsObj.html
+        return titulo
+    except:
+        print("Ocorreu um erro no conteudo da pagina")
+        return None
+
+
+
+
+titulo = input("Informe a URL :")
+palavra = input("Informe a palavra a ser procurada")
+
+obj = getElementWEb(titulo)
+
+word_re = re.compile(palavra,re.IGNORECASE)
+
+word_All = re.findall(word_re,str(obj))
+print(word_All)
